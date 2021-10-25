2d865aae8fd63bd3053b5c872b8d1a98c8594e68
Jared Nightingale <jaredthomasnightingale@gmail.com>
2021-10-24 23:26:13 -0500

    Added randomization functionality

--- src/models/item.py
+++ src/models/item.py
@@ -10,11 +10,12 @@ class Item:
     tier: int
     price: int
     ident: int
+    childIdent: int
     is_active: bool
     is_starter: bool
     type: ItemType
-    roles: List[Role] = []
-    attributes: dict = {}
+    roles: List[Role]
+    attributes: dict
 
     def __init__(self, json):
         self.name = json['DeviceName']
@@ -23,13 +24,27 @@ class Item:
         self.tier = int(json['ItemTier'])
         self.price = int(json['Price'])
         self.ident = int(json['ItemId'])
+        self.childIdent = int(json['ChildItemId'])
         self.is_active = json['ActiveFlag'] == 'y'
         self.is_starter = json['StartingItem']
         self.type = ItemType[json['Type']]
+        self.roles = self.get_roles(json['RestrictedRoles'])
         self.attributes = self.get_attributes(json['ItemDescription']['Menuitems'])
 
-    def get_roles(self, roles_str):
-        pass
+    @staticmethod
+    def get_roles(roles_str: str) -> List[Role]:
+        roles = []
+        role_list = roles_str.split(',')
+        for role in role_list:
+            try:
+                roles.append(Role[role.capitalize()])
+            except KeyError:
+                roles += [role for role in Role]
+        return roles
 
-    def get_attributes(self, menu_items: dict) -> dict:
-        pass
+    @staticmethod
+    def get_attributes(menu_items: dict) -> dict:
+        attributes: dict = {}
+        for entry in menu_items:
+            attributes[entry['Description']] = entry['Value']
+        return attributes
--- src/randomizer.py
+++ src/randomizer.py
@@ -1,24 +1,69 @@
 import json
-from typing import List
+from typing import List, Tuple
 
+from random import choice, sample
+from src.models.enums import ItemType, Role
 from src.models.god import God
 from src.models.item import Item
 
-gods_json = json.load(open('../data/gods.json'))
-items_json = json.load(open('../data/items.json'))
 
+def get_random_starter_items() -> Tuple[Item, Item]:
+    starter_evolved = choice([i for i in items if i.is_starter and i.tier == 2])
+    starter = next((i for i in items if i.ident == starter_evolved.childIdent), None)
+    return starter, starter_evolved
+
+
+def get_random_items(role: Role) -> List[Item]:
+    sample_set = [i for i in items if
+                  i.type == ItemType.Item and
+                  i.tier == 3 and
+                  i.is_active and
+                  role in i.roles]
+    return sample(sample_set, 5)
+
+
+def get_random_relics() -> List[Item]:
+    return sample([i for i in items if i.type is ItemType.Active and i.tier == 2], 2)
+
+
+def get_random_consumables() -> List[Item]:
+    return sample([i for i in items if i.type is ItemType.Consumable], 2)
+
+
+def filter_items_by_active() -> List[Item]:
+    return [i for i in items if i.is_active]
+
+
+# Create lists to hold our data
 gods: List[God] = []
 items: List[Item] = []
-types: set = set()
 
-for god in gods_json:
+# Read in all god data and parse into God objects
+for god in json.load(open('../data/gods.json')):
     gods.append(God(god))
 
-for item in items_json:
-    types.add(item['Type'])
+# Read in all item data and parse into Item objects
+for item in json.load(open('../data/items.json')):
     items.append(Item(item))
 
-for item in items:
-    print(item.__dict__)
+# Filter items by active
+items = filter_items_by_active()
 
-print(types)
+god = choice(gods)
+starters = get_random_starter_items()
+
+print(f'God:\n{god.name}')
+print()
+print(f'Items:\n{starters[0].name} -> {starters[1].name}')
+for item in get_random_items(god.role):
+    print(f'{item.name} : {item.attributes}')
+
+print()
+print("Relics:")
+for relic in get_random_relics():
+    print(relic.name)
+
+print()
+print("Consumables:")
+for consumable in get_random_consumables():
+    print(consumable.name)
