0428f744e5f41fc4bcf95cf5935f2caeac69a5eb
Jared Nightingale <jaredthomasnightingale@gmail.com>
2021-10-29 15:27:52 -0500

    Added special logic for Baron's Brew and Rat

--- src/randomizer.py
+++ src/randomizer.py
@@ -8,17 +8,29 @@ from src.models.item import Item
 
 
 def get_random_starter_items() -> Tuple[Item, Item]:
+    # Pick from the pool of enhanced starter items
     starter_evolved = choice([i for i in items if i.is_starter and i.tier == 2])
+    # Get the basic starter item based on the ChildId of the enhanced starter item
     starter = next((i for i in items if i.ident == starter_evolved.childIdent), None)
     return starter, starter_evolved
 
 
-def get_random_items(role: Role) -> List[Item]:
-    sample_set = [i for i in items if
+def get_random_items(_god: God) -> List[Item]:
+    random_item_pool = items
+    # If god is Baron, remove Baron's Brew from the item list
+    if 'Baron' in _god.name:
+        random_item_pool = [i for i in random_item_pool if 'Baron' not in i.name]
+
+    if 'Ratatoskr' in _god.name:
+        random_item_pool = [i for i in random_item_pool if 'Acorn' not in i.name]
+
+    print(len(random_item_pool))
+
+    sample_set = [i for i in random_item_pool if
                   i.type == ItemType.Item and
                   i.tier == 3 and
                   i.is_active and
-                  role in i.roles]
+                  _god.role in i.roles]
     return sample(sample_set, 5)
 
 
@@ -50,20 +62,22 @@ for item in json.load(open('../data/items.json')):
 items = filter_items_by_active()
 
 god = choice(gods)
-starters = get_random_starter_items()
-
 print(f'God:\n{god.name}')
 print()
+
+starters = get_random_starter_items()
 print(f'Items:\n{starters[0].name} -> {starters[1].name}')
-for item in get_random_items(god.role):
+for item in get_random_items(god):
     print(f'{item.name} : {item.attributes}')
 
 print()
+
 print("Relics:")
 for relic in get_random_relics():
     print(relic.name)
 
 print()
+
 print("Consumables:")
 for consumable in get_random_consumables():
     print(consumable.name)
